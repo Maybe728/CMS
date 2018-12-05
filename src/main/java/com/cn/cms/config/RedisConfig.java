1bde8df1644b7004f9afce3dd0ac71acc1cabf3a
Maybe728 <1007402145@qq.com>
2018-12-05 16:38:44 +0800

    first commit

---
+++ src/main/java/com/cn/cms/config/RedisConfig.java
@@ -0,0 +1,131 @@
+package com.cn.cms.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.time.Duration;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2018/11/27 10:44
+ * @Description:
+ */
+@Configuration
+@EnableAutoConfiguration
+@PropertySource(value = { "classpath:redis.properties" })
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Value("${redis.database}")
+    private int database;
+
+    @Value("${redis.host}")
+    private String hostName;
+
+    @Value("${redis.port}")
+    private int port;
+
+    @Value("${redis.pass}")
+    private String password;
+
+    @Value("${redis.timeout}")
+    private int timeout;
+
+    @Value("${redis.maxIdle}")
+    private int maxIdle;
+
+    @Value("${redis.minIdle}")
+    private int minIdle;
+
+    @Value("${redis.maxTotal}")
+    private int maxTotal;
+
+    @Value("${redis.maxWaitMillis}")
+    private long maxWaitMillis;
+
+    @Value("${redis.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    public JedisPoolConfig poolCofig(int maxIdle, int maxTotal,
+                                     long maxWaitMillis, boolean testOnBorrow) {
+        JedisPoolConfig poolCofig = new JedisPoolConfig();
+        poolCofig.setMaxIdle(maxIdle);
+        poolCofig.setMaxIdle(minIdle);
+        poolCofig.setMaxTotal(maxTotal);
+        poolCofig.setMaxWaitMillis(maxWaitMillis);
+        poolCofig.setTestOnBorrow(testOnBorrow);
+        return poolCofig;
+    }
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setDatabase(database);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+
+        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+        jedisClientConfiguration.connectTimeout(Duration.ofMillis(timeout));//  connection timeout
+
+        JedisConnectionFactory factory = new JedisConnectionFactory(redisStandaloneConfiguration,
+                jedisClientConfiguration.build());
+        return factory;
+    }
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator() {
+        return (target, method, objects) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append("::" + method.getName() + ":");
+            for (Object obj : objects) {
+                sb.append(obj.toString());
+            }
+            return sb.toString();
+        };
+    }
+
+//	    @Bean
+//	    public CacheManager cacheManager(@SuppressWarnings("rawtypes") RedisTemplate redisTemplate) {
+//	        return new RedisCacheManager(redisTemplate);
+//	    }
+
+//    @Bean
+//    public RedisTemplate<String, String> redisTemplate(
+//            JedisConnectionFactory factory) {
+//        StringRedisTemplate template = new StringRedisTemplate(factory);
+//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+//        ObjectMapper om = new ObjectMapper();
+//        //om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//        jackson2JsonRedisSerializer.setObjectMapper(om);
+//        template.setValueSerializer(jackson2JsonRedisSerializer);
+//        template.afterPropertiesSet();
+//        return template;
+//    }
+
+    @Bean
+    public JedisPool getJedisPool() {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(maxTotal);
+        config.setMaxIdle(maxIdle);
+        config.setMinIdle(minIdle);
+        config.setTestOnBorrow(testOnBorrow);
+        config.setTestOnReturn(false);
+        config.setBlockWhenExhausted(true);
+        config.setMaxWaitMillis(maxWaitMillis);
+        JedisPool pool = new JedisPool(config, hostName, port, timeout, password);
+        return pool;
+    }
+}
---
+++ src/main/java/com/cn/cms/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.cn.cms.dao;
+
+import com.cn.cms.dto.UserInfo;
+
+/**
+ * <p>
+ * 系统用户表 Mapper 接口
+ * </p>
+ *
+ * @author Auto Generator
+ * @since 2018-07-16
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+    UserInfo findUserInfo(String userName);
+
+}
